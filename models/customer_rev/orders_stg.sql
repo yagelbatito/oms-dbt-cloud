e2b3d0c253d0acb715f7ef1caeab74d2edecb838
yagel batito <yagelbbb@gmail.com>
2026-05-20 13:40:46 +0000

    20

---
+++ models/customer_rev/orders_stg.sql
@@ -0,0 +1,21 @@
+SELECT
+    OrderID,
+    OrderDate,
+    CustomerID,
+    EmployeeID,
+    StoreID,
+    Status AS StatusCD,
+    CASE
+        WHEN Status = '01' THEN 'In Progress'
+        WHEN Status = '02' THEN 'Completed'
+        WHEN Status = '03' THEN 'Cancelled'
+        ELSE NULL
+    END AS StatusDesc,
+    CASE
+        WHEN StoreID = 1000 THEN 'Online'
+        ELSE 'In-store'
+    END AS ORDER_CHANNEL,
+    Updated_at,
+    current_timestamp as dbt_updated_at
+FROM
+    {{ source('landing', 'orders') }}
